af1f7daa66061aea30fa4845f16e6c6a13943449
Adrian Palacios <78890986+AdrianP17@users.noreply.github.com>
2025-08-10 12:53:42 -0500

    Update CORS configuration and change default port to 3001

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+        origin: 'http://localhost:3000',
+      credentials: true
+      })
   const config = new DocumentBuilder()
     .setTitle('Task manager Api Documentation')
     .setDescription('CRUD for tasks and authentication')
@@ -12,6 +16,6 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
